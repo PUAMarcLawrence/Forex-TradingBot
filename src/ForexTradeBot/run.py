23f29b32684882a88b431c8551f9292232795195
PUAMarcLawrence <83485910+PUAMarcLawrence@users.noreply.github.com>
2024-05-17 07:14:08 +0800

    Update

--- src/ForexTradeBot/run.py
+++ src/ForexTradeBot/run.py
@@ -2,9 +2,9 @@ import subprocess
 
 venv_python = r'.venv\Scripts\python.exe'
 # Start the first program
-p1 = subprocess.Popen([venv_python, 'src\ForexTradeBot\TradingB_BackEnd.py'])
+p1 = subprocess.Popen([venv_python, "S:\Programs\Trading Bot\ForexTradingBotV3\src\ForexTradeBot\TradingB_BackEnd.py"])
 # Start the second program
-p2 = subprocess.Popen([venv_python, 'src\ForexTradeBot\TradingB_FrontEnd.py'])
+p2 = subprocess.Popen([venv_python, "S:\Programs\Trading Bot\ForexTradingBotV3\src\ForexTradeBot\TradingB_FrontEnd.py"])
 
 # Wait for both programs to complete
 p1.wait()
