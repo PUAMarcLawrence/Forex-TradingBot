205b6c0c8234ceaad59ba8e1abfbaabe40b0b992
PUAMarcLawrence <83485910+PUAMarcLawrence@users.noreply.github.com>
2024-05-23 12:28:13 +0800

    Update

--- forextradebotv3/run.py
+++ forextradebotv3/run.py
@@ -1,10 +1,12 @@
 import subprocess
 
 venv_python = r'.venv\Scripts\python.exe'
+
 # Start the first program
-p1 = subprocess.Popen([venv_python, "forextradebotv3\TradingB_BackEnd.py"])
+p1 = subprocess.Popen([venv_python, "forextradebotv3\TradingB_FrontEnd.py"])
 # Start the second program
-p2 = subprocess.Popen([venv_python, "forextradebotv3\TradingB_FrontEnd.py"])
+p2 = subprocess.Popen([venv_python, "forextradebotv3\TradingB_BackEnd.py"])
+
 
 # Wait for both programs to complete
 p1.wait()
